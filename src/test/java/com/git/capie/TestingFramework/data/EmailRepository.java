b304405d18af261329f316cda47ab544a988e428
CAPie <strilchuk.work@gmail.com>
2015-03-26 17:11:23 +0200

    uncheck coments

--- src/test/java/com/git/capie/TestingFramework/data/EmailRepository.java
+++ src/test/java/com/git/capie/TestingFramework/data/EmailRepository.java
@@ -9,155 +9,155 @@ public class EmailRepository {
 
 	public static List<String> getCompaniesHREmailesInLviv() {
 		List<String> companiesHREmailesInLviv = new ArrayList<String>();
-//		// Eleks
-//		companiesHREmailesInLviv.add("svitlana.rotar@eleks.com");
-//
-//		// Lohika
-//		companiesHREmailesInLviv.add("pbaluk@lohika.com");
-//
-//		// Epam
-//		companiesHREmailesInLviv.add("WFARecruitingLviv@epam.com");
-//
-//		// DataArt
-//		companiesHREmailesInLviv.add("hr-lviv@dataart.com");
-//
-//		// GlobalLogic
-//		companiesHREmailesInLviv.add("jobs.lviv@globallogic.com");
-//
-//		// Intelias
-//		companiesHREmailesInLviv.add("job@intellias.com");
-//
-//		// SvitlaSystems
-//		companiesHREmailesInLviv.add("responses@svitla.com");
-//
-//		// SigmaSoftware
+		// Eleks
+		companiesHREmailesInLviv.add("svitlana.rotar@eleks.com");
+
+		// Lohika
+		companiesHREmailesInLviv.add("pbaluk@lohika.com");
+
+		// Epam
+		companiesHREmailesInLviv.add("WFARecruitingLviv@epam.com");
+
+		// DataArt
+		companiesHREmailesInLviv.add("hr-lviv@dataart.com");
+
+		// GlobalLogic
+		companiesHREmailesInLviv.add("jobs.lviv@globallogic.com");
+
+		// Intelias
+		companiesHREmailesInLviv.add("job@intellias.com");
+
+		// SvitlaSystems
+		companiesHREmailesInLviv.add("responses@svitla.com");
+
+		// SigmaSoftware
 		companiesHREmailesInLviv.add("team@sigma.software");
 
-//		// Itera
-//		companiesHREmailesInLviv.add("Svetlana.Artemenko@iteraconsulting.com");
-//
-//		// TODO Mirantis
-//		// companiesHREmailesInLviv.add("");
-//
-//		// TODO SymphonySolutions
-//		companiesHREmailesInLviv.add("jobs@symphony-solutions.eu");
-//
-//		// TODO N-iX
-//		// companiesHREmailesInLviv.add("");
-//
-//		// TEAM_International
-//		companiesHREmailesInLviv.add("Galyna.Hyrka@teaminternational.com");
-//
-//		// WaverlaySoftware
-//		companiesHREmailesInLviv.add(" jobs-ua@waverleysoftware.com");
-//
-//		// iLogos
-//		companiesHREmailesInLviv.add("jobs_ua@ilogos.biz");
-//
-//		// AlterSoft
-//		companiesHREmailesInLviv.add("job@altexsoft.com");
-//
-//		// OSFglobal
-//		companiesHREmailesInLviv.add("hr@osf-global.com");
-//
-//		// EdvantisSoftware
-//		companiesHREmailesInLviv.add("job@edvantis.com");
-//
-//		// BinaryStudio
-//		companiesHREmailesInLviv.add("job@binary-studio.com");
-//
-//		// TODO Perfectial
-//		// companiesHREmailesInLviv.add("");
-//
-//		// GeeksForLess
-//		companiesHREmailesInLviv.add("hr@geeksforless.net");
-//
-//		// UnkurSolutions
-//		companiesHREmailesInLviv.add("job@unkur.com");
-//
-//		// TODO BlinkReaction
-//		// companiesHREmailesInLviv.add("");
-//
-//		// TODO HandSome
-//		// companiesHREmailesInLviv.add("");
-//
-//		// InterLogic
-//		companiesHREmailesInLviv.add("hr@interlogic.com.ua");
-//
-//		// RomexSoft
-//		companiesHREmailesInLviv.add("jobs@romexsoft.com");
-//
-//		// FrezePro
-//		companiesHREmailesInLviv.add("hr@freezeprosoftware.com");
-//
-//		// Malkos
-//		companiesHREmailesInLviv.add("recruiting@malkosua.com");
-//
-//		// TemplateMonster
-//		companiesHREmailesInLviv.add("hr@templatemonster.com");
-//
-//		// TODO JetThoughts
-//		// companiesHREmailesInLviv.add("");
-//
-//		// TODO Intelliarts
-//		// companiesHREmailesInLviv.add("");
-//
-//		// TODO EnvionSoftware
-//		// companiesHREmailesInLviv.add("envioninfo@envionsoftware.com");
-//
-//		// internetDevels
-//		companiesHREmailesInLviv.add("anya@internetdevels.com");
-//
-//		// TeachMagic
-//		companiesHREmailesInLviv.add("oleg@techmagic.co");
-//
-//		// TicketsUA
-//		companiesHREmailesInLviv.add("hr@tickets.ua");
-//
-//		// Satelliz
-//		companiesHREmailesInLviv.add("recrutement@satelliz.com");
-//
-//		// ViSoft
-//		companiesHREmailesInLviv.add("ukrinfo@visoft.de");
-//
-//		// I-Nova
-//		companiesHREmailesInLviv.add("orest.chaykivskyy@i-novaconsulting.com");
-//
-//		// conscensia
-//		companiesHREmailesInLviv.add("careers@conscensia.com");
-//
-//		// neadevis
-//		companiesHREmailesInLviv.add("igor.luzhanskiy@neadevis.com");
-//
-//		// TODO SoftBistro
-//		// companiesHREmailesInLviv.add("");
-//
-//		// Ericpol
-//		companiesHREmailesInLviv.add("job_ua@ericpol.com");
-//
-//		// Leona
-//		companiesHREmailesInLviv.add("mail@leona.lviv.ua");
-//
-//		// IndeemaSoftware
-//		companiesHREmailesInLviv.add("support@indeema.com");
-//
-//		// UkeessSoftware
-//		companiesHREmailesInLviv.add("jobs@ukeess.com");
-//
-//		// ExoftSoftware
-//		companiesHREmailesInLviv.add("jobs@exoft.net");
-//
-//		// StreamSoft
-//		companiesHREmailesInLviv.add("job@streamsoft.org");
-//
-//		// VindiTek
-//		companiesHREmailesInLviv.add("contact@vinditek.com");
-//
-//		// ABTO_Softvare
-//		companiesHREmailesInLviv.add("jobs@abtosoftware.com");
-//
-//		// TODO LimelightNetworks
-//		 companiesHREmailesInLviv.add("");
+		// Itera
+		companiesHREmailesInLviv.add("Svetlana.Artemenko@iteraconsulting.com");
+
+		// TODO Mirantis
+		// companiesHREmailesInLviv.add("");
+
+		// TODO SymphonySolutions
+		companiesHREmailesInLviv.add("jobs@symphony-solutions.eu");
+
+		// TODO N-iX
+		// companiesHREmailesInLviv.add("");
+
+		// TEAM_International
+		companiesHREmailesInLviv.add("Galyna.Hyrka@teaminternational.com");
+
+		// WaverlaySoftware
+		companiesHREmailesInLviv.add(" jobs-ua@waverleysoftware.com");
+
+		// iLogos
+		companiesHREmailesInLviv.add("jobs_ua@ilogos.biz");
+
+		// AlterSoft
+		companiesHREmailesInLviv.add("job@altexsoft.com");
+
+		// OSFglobal
+		companiesHREmailesInLviv.add("hr@osf-global.com");
+
+		// EdvantisSoftware
+		companiesHREmailesInLviv.add("job@edvantis.com");
+
+		// BinaryStudio
+		companiesHREmailesInLviv.add("job@binary-studio.com");
+
+		// TODO Perfectial
+		// companiesHREmailesInLviv.add("");
+
+		// GeeksForLess
+		companiesHREmailesInLviv.add("hr@geeksforless.net");
+
+		// UnkurSolutions
+		companiesHREmailesInLviv.add("job@unkur.com");
+
+		// TODO BlinkReaction
+		// companiesHREmailesInLviv.add("");
+
+		// TODO HandSome
+		// companiesHREmailesInLviv.add("");
+
+		// InterLogic
+		companiesHREmailesInLviv.add("hr@interlogic.com.ua");
+
+		// RomexSoft
+		companiesHREmailesInLviv.add("jobs@romexsoft.com");
+
+		// FrezePro
+		companiesHREmailesInLviv.add("hr@freezeprosoftware.com");
+
+		// Malkos
+		companiesHREmailesInLviv.add("recruiting@malkosua.com");
+
+		// TemplateMonster
+		companiesHREmailesInLviv.add("hr@templatemonster.com");
+
+		// TODO JetThoughts
+		// companiesHREmailesInLviv.add("");
+
+		// TODO Intelliarts
+		// companiesHREmailesInLviv.add("");
+
+		// TODO EnvionSoftware
+		// companiesHREmailesInLviv.add("envioninfo@envionsoftware.com");
+
+		// internetDevels
+		companiesHREmailesInLviv.add("anya@internetdevels.com");
+
+		// TeachMagic
+		companiesHREmailesInLviv.add("oleg@techmagic.co");
+
+		// TicketsUA
+		companiesHREmailesInLviv.add("hr@tickets.ua");
+
+		// Satelliz
+		companiesHREmailesInLviv.add("recrutement@satelliz.com");
+
+		// ViSoft
+		companiesHREmailesInLviv.add("ukrinfo@visoft.de");
+
+		// I-Nova
+		companiesHREmailesInLviv.add("orest.chaykivskyy@i-novaconsulting.com");
+
+		// conscensia
+		companiesHREmailesInLviv.add("careers@conscensia.com");
+
+		// neadevis
+		companiesHREmailesInLviv.add("igor.luzhanskiy@neadevis.com");
+
+		// TODO SoftBistro
+		// companiesHREmailesInLviv.add("");
+
+		// Ericpol
+		companiesHREmailesInLviv.add("job_ua@ericpol.com");
+
+		// Leona
+		companiesHREmailesInLviv.add("mail@leona.lviv.ua");
+
+		// IndeemaSoftware
+		companiesHREmailesInLviv.add("support@indeema.com");
+
+		// UkeessSoftware
+		companiesHREmailesInLviv.add("jobs@ukeess.com");
+
+		// ExoftSoftware
+		companiesHREmailesInLviv.add("jobs@exoft.net");
+
+		// StreamSoft
+		companiesHREmailesInLviv.add("job@streamsoft.org");
+
+		// VindiTek
+		companiesHREmailesInLviv.add("contact@vinditek.com");
+
+		// ABTO_Softvare
+		companiesHREmailesInLviv.add("jobs@abtosoftware.com");
+
+		// TODO LimelightNetworks
+		 companiesHREmailesInLviv.add("");
 
 		return companiesHREmailesInLviv;
 	}
