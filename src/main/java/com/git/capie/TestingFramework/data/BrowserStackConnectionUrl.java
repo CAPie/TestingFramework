a8a26ee9cc315f90de733c1ae2c55ebe88ea71d5
CAPie <strilchuk.work@gmail.com>
2015-05-01 17:31:04 +0300

    BrowserStack connection url

--- src/main/java/com/git/capie/TestingFramework/data/BrowserStackConnectionUrl.java
+++ src/main/java/com/git/capie/TestingFramework/data/BrowserStackConnectionUrl.java
@@ -5,8 +5,8 @@ public class BrowserStackConnectionUrl {
 	private static final String URL = "http://%s:%s@hub.browserstack.com/wd/hub";
 
 	public static String getURL() {
-		String username = "nicolaepasecinic1";
-		String key = "ZVpkfzzeNKBfVFEryRoJ";
+		  String username = "denis65";
+		  String key = "PcFjENv5gmGa6TVbzvUg";
 		return String.format(URL, username, key);
 	}
 }
